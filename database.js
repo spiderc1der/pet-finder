4fdd3b838d471066b8903cbe44f4d4bfc87cfd7a
Merone Delnesa <mtd6620@rit.edu>
2023-11-10 00:16:18 -0500

    fixed displaying of pet details, added text to webpage

--- database.js
+++ database.js
@@ -1,25 +1,30 @@
-let EXAMPLE_URL = "\"" + "https://static.chickensmoothie.com/archive/image.php?k=C5057EC610648C14A89E48D89EC4656A&bg=ffffff" + "\"";
-
-
-let data = JSON.stringify({
-    "dataSource": "CSpets",
-    "database": "csPets",
-    "collection": "pets",
-    "filter": {
-      "_id": "C5057EC610648C14A89E48D89EC4656A"
-    }
-  });
-  
-  
 
 async function handleRequest() {
 
   // check validity of link entered  
   var input = document.getElementById("petLink").value;
   if (input.includes("https://static.chickensmoothie.com/archive/image.php?k=")) {
-    document.getElementById("result").innerHTML = "Valid link!";
+    
+    //document.getElementById("result").innerHTML = "Valid link!";
 
-  
+    // extract ID from link
+    let temp1 = input.replace("https://static.chickensmoothie.com/archive/image.php?k=", "");
+    let temp2 = temp1.replace("&bg", " ");
+    let temp3 = temp2.split(" ");
+    let petID = temp3[0];
+
+    console.log(petID);
+
+    let data = JSON.stringify({
+      "dataSource": "CSpets",
+      "database": "csPets",
+      "collection": "pets",
+      "filter": {
+        "_id": petID
+      }
+    });
+
+    search(data, input, petID);
 
   }
   else if (input == "") {
@@ -29,12 +34,9 @@ async function handleRequest() {
     document.getElementById("result").innerHTML = "Sorry, this is an invalid image address. Please try again.";
   }
 
-
 }
 
-
-
-async function search() {
+async function search(data, imgURL, pID) {
   try {
 
     // get authorization token 
@@ -50,7 +52,9 @@ async function search() {
 
       let token = response.data.access_token; 
 
-      let c2 = {
+      // query database
+
+      let config = {
         method: 'post',
         maxBodyLength: Infinity,
         url: 'https://us-east-2.aws.data.mongodb-api.com/app/data-qmnzi/endpoint/data/v1/action/findOne',
@@ -63,37 +67,164 @@ async function search() {
 
       // test output
 
-      axios(c2).then(function (response) {
+      axios(config).then(function (response) {
         console.log(JSON.stringify(response.data));
 
-        console.log("dfsfsdfdsfds");
+        //document.getElementById("result").innerHTML = response.data.document.pet_name;
 
-        let name = JSON.stringify(response.data.document.pet_name);
-        console.log(name);
 
-        document.getElementById("result").innerHTML = response.data.document.pet_name;
+        // check for a null response 
+        if (response.data.document == null) {
 
-        var pet = JSON.stringify(response.data.document.pet_name).replace(/^"(.*)"$/, '$1');
-        var month = JSON.stringify(response.data.document.month).replace(/^"(.*)"$/, '$1');
-        var year = JSON.stringify(response.data.document.year).replace(/^"(.*)"$/, '$1');
-        var species = JSON.stringify(response.data.document.species).replace(/^"(.*)"$/, '$1');
-        var artist = JSON.stringify(response.data.document.artist_name).replace(/^"(.*)"$/, '$1');
-        var artist_l = JSON.stringify(response.data.document.artist_link);
-        var litter = JSON.stringify(response.data.document.litter_name).replace(/^"(.*)"$/, '$1');
-        var archive_l = JSON.stringify(response.data.document.archive_link); 
+          // search database to see if it's a newborn ID 
+
+          let data2 = JSON.stringify({
+            "dataSource": "CSpets",
+            "database": "csPets",
+            "collection": "pets",
+            "filter": {
+              "newborn_id": pID
+            }
+          });
+
+          let config2 = {
+            method: 'post',
+            maxBodyLength: Infinity,
+            url: 'https://us-east-2.aws.data.mongodb-api.com/app/data-qmnzi/endpoint/data/v1/action/find',
+            headers: { 
+              'Content-Type': 'application/json', 
+              'Authorization': token
+            },
+            data : data2
+          };
+
+          // search all pets with the given ID as their newborn ID 
+          axios(config2).then(function (response2) {
+
+            // if none are found, update with message
+            if (response2.data.documents == null) {
+              document.getElementById("result").innerHTML =
+                "Sorry, the given pet either does not exist or has yet to be added to the database.";
+            }
+
+            else {
+              let results = response2.data.documents;
+              let IDS = [];
+              let pet_names = [];
+
+              // get values consistent across the litter from the first 
+              // pet in the array 
+              let month = results[0].month;
+              let year = results[0].year;
+              let litter = results[0].litter_name;
+              let archive_l = results[0].archive_link; 
+
+              results.forEach(element => {
+                // add each pet's information to its respective array 
+                IDS.push(element._id);
+                pet_names.push(element.pet_name);
+                
+              });
+
+              if (IDS.length == 1) {
+                // only one outcome for the newborn
+
+                let name = JSON.stringify(results[0].pet_name).replace(/^"(.*)"$/, '$1');
+                let pps = JSON.stringify(results[0].PPS).replace(/^"(.*)"$/, '$1');
+                pps = pps.toLowerCase().charAt(0).toUpperCase() + pps.slice(1).toLowerCase();
+                let species = JSON.stringify(results[0].species).replace(/^"(.*)"$/, '$1');
+                species = species.toLowerCase().charAt(0).toUpperCase() + species.slice(1).toLowerCase();
+                let artist = JSON.stringify(results[0].artist_name).replace(/^"(.*)"$/, '$1');
+                let artist_l = JSON.stringify(results[0].artist_link);
+
+                document.getElementById("result").innerHTML =
+                "<img src = " + imgURL + "> <br />" +
+                "<a href = " + archive_l + ">" + "Archive link" + "</a> <br />" +
+                "<b>Pet name: </b>" + name +
+                "<br /> <b>Release date: </b>" + month + " " + year +
+                "<br /> <b>Species: </b>" + species + "<br /> Litter name: " + litter +
+                "<br /> <b>PPS: </b>" + pps + 
+                "<br /> <b>Artist: </b> <a href = " + artist_l + "> " + artist + " </a>";
+          
+
+              }
+              else {
+                // must display all pets
+
+                let addresses = [];
+
+                IDS.forEach(element => {
+                  // construct addresses and add to array 
+                  addresses.push("https://static.chickensmoothie.com/archive/image.php?k=" + element + "&bg=99c57c");
+                });
+                
+                // construct info blurb 
+                let output = "<b>Potential outcomes:</b> <br />";
+                addresses.forEach(element => { 
+                  output += "<img src = " + element + ">";
+                });
+                output += "<br /> <a href = " + archive_l + ">" + "Archive link" +
+                  "</a> <br /> <b>Litter name</b>: " + litter + "<br /> <b>Pet names:</b> ";
+                for (let i = 0; i < pet_names.length; i++){
+                  output += pet_names[i] + ", ";
+                }
+                // remove last comma
+                output = output.substring(0, output.length - 2);
+                output += "<br /> <b>Release date:</b> " + month + " " + year;
+
+                document.getElementById("result").innerHTML = output;
+
+
+              }
+
+            
+            }
+
+
+          })
+            .catch(function (error2) {
+              console.log(error2);
+          });
+          
         
 
-        document.getElementById("result").innerHTML = "<img src = " + EXAMPLE_URL + "> <br />" +
-          "<a href = " + archive_l + ">" + "Archive link" + "</a> <br />" + "Release date: " + month + " " + year + "<br />Pet name: " + pet +
-          "<br /> Litter name: " + litter + "<br /> Species: " + species +
-          "<br /> Artist: <a href = " + artist_l + "> " + artist + " </a>";
+        }
+        else {
+
+          // format pet info
+
+          var pet = JSON.stringify(response.data.document.pet_name).replace(/^"(.*)"$/, '$1');
+          var month = JSON.stringify(response.data.document.month).replace(/^"(.*)"$/, '$1');
+          var year = JSON.stringify(response.data.document.year).replace(/^"(.*)"$/, '$1');
+          var pps = JSON.stringify(response.data.document.PPS).replace(/^"(.*)"$/, '$1');
+          pps = pps.toLowerCase().charAt(0).toUpperCase() + pps.slice(1).toLowerCase();
+          var species = JSON.stringify(response.data.document.species).replace(/^"(.*)"$/, '$1');
+          species = species.toLowerCase().charAt(0).toUpperCase() + species.slice(1).toLowerCase();
+          var artist = JSON.stringify(response.data.document.artist_name).replace(/^"(.*)"$/, '$1');
+          var artist_l = JSON.stringify(response.data.document.artist_link);
+          var litter = JSON.stringify(response.data.document.litter_name).replace(/^"(.*)"$/, '$1');
+          var archive_l = JSON.stringify(response.data.document.archive_link); 
+          
+          // display pet info 
+
+          document.getElementById("result").innerHTML =
+            "<img src = " + imgURL + "> <br />" +
+            "<a href = " + archive_l + ">" + "Archive link" + "</a> <br />" +
+            "<b>Pet name: </b>" + pet +
+            "<br /> <b>Release date: </b>" + month + " " + year +
+            "<br /> <b>Species: </b>" + species + "<br /> Litter name: " + litter +
+            "<br /> <b>PPS: </b>" + pps + 
+            "<br /> <b>Artist: </b> <a href = " + artist_l + "> " + artist + " </a>";
+          
+        }
 
 
+        
+        
       })
         .catch(function (error) {
           console.log(error)
         });
-
 
     })
       .catch(function (error) {
